155061f8eec3c37a60a6d6500926af9e91dbf142
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2024-04-02 19:08:38 +0200

    Add font family and basic colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -8,13 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        monserrat: ["var(--font-monserrat)"],
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
